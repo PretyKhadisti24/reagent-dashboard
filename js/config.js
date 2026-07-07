ce61453e75caa2003dde8772e351bc34c6554ed5
Pretty24 <161545453+PretyKhadisti24@users.noreply.github.com>
2026-07-07 11:06:54 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,84 @@
+/*
+=========================================================
+LIMS Lite
+Lab Inventory Management System
+Version : 2.3.0
+=========================================================
+*/
+
+const CONFIG = {
+
+    APP: {
+        NAME: "LIMS Lite",
+        FULLNAME: "Lab Inventory Management System",
+        VERSION: "2.3.0",
+        AUTHOR: "Gyra Project"
+    },
+
+    API: {
+        BASE_URL: "https://script.google.com/macros/s/AKfycbzFKo3vajXoH7qs84mbQ85gGk1bFGBNkXXeedKyXl7NyRJh_Z_HQdzyfa3hIl0FUB0/exec",
+        TIMEOUT: 15000
+    },
+
+    SHEETS: {
+
+        INVENTORY: "Master Stok",
+
+        RECEIVING: "Penerimaan",
+
+        DISTRIBUTION: "Distribusi",
+
+        DISPOSAL: "Pemusnahan",
+
+        DOCUMENT: "Document Index",
+
+        STOCK_CARD: "Kartu Stok"
+
+    },
+
+    STATUS: {
+
+        ACTIVE: "AKTIF",
+
+        INACTIVE: "TIDAK AKTIF"
+
+    },
+
+    STORAGE: {
+
+        LOCAL_KEY: "lims-lite-cache",
+
+        LAST_SYNC: "lims-lite-last-sync"
+
+    }
+
+};
+
+
+/*
+=========================================================
+Global State
+=========================================================
+*/
+
+const STATE = {
+
+    inventory: [],
+
+    receiving: [],
+
+    distribution: [],
+
+    disposal: [],
+
+    documents: [],
+
+    stockCard: [],
+
+    dashboard: {},
+
+    online: false,
+
+    lastSync: null
+
+};
